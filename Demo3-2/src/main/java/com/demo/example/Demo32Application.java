499fc57ae6ee1ebdc361e54818774b0335a852d2
Pallavi2403 <Pallavivenugopal.letsgettin.com>
2019-06-06 17:09:46 +0530

    done by pallavi

---
+++ Demo3-2/src/main/java/com/demo/example/Demo32Application.java
@@ -0,0 +1,15 @@
+package com.demo.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo32Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo32Application.class, args);
+		//branches
+		//done by pallavi
+	}
+
+}
